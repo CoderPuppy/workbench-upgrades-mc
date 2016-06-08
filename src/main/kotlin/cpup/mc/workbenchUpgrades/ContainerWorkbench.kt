17c7a77d21b648cc90963cf1445261ea68b3a227
CoderPuppy <coderpup@gmail.com>
2016-06-08 15:23:27 -0400

    Make the multiplicatoin configurable

--- src/main/kotlin/cpup/mc/workbenchUpgrades/ContainerWorkbench.kt
+++ src/main/kotlin/cpup/mc/workbenchUpgrades/ContainerWorkbench.kt
@@ -9,16 +9,31 @@ import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 
 class ContainerWorkbench(inv: InventoryPlayer, private val world: World, private val pos: BlockPos): ContainerWorkbench(inv, world, pos) {
+
+		override fun canInteractWith(p_canInteractWith_1_: EntityPlayer): Boolean {
+		return if (world.getBlockState(this.pos).block !== WorkbenchUpgrades.block) false else p_canInteractWith_1_.getDistanceSq(this.pos.x.toDouble() + 0.5, this.pos.y.toDouble() + 0.5, this.pos.z.toDouble() + 0.5) <= 64.0
+	}
+
+	var modifier = 1
+
 	override fun onCraftMatrixChanged(inv: IInventory?) {
 		var stack = CraftingManager.getInstance().findMatchingRecipe(craftMatrix, world)
 		if(stack != null) {
 			stack = stack.copy()
-			stack.stackSize *= 4;
+			stack.stackSize *= modifier;
 		}
 		craftResult.setInventorySlotContents(0, stack)
 	}
 
-	override fun canInteractWith(p_canInteractWith_1_: EntityPlayer): Boolean {
-		return if (world.getBlockState(this.pos).block !== WorkbenchUpgrades.block) false else p_canInteractWith_1_.getDistanceSq(this.pos.x.toDouble() + 0.5, this.pos.y.toDouble() + 0.5, this.pos.z.toDouble() + 0.5) <= 64.0
+	override fun enchantItem(playerIn: EntityPlayer?, id: Int): Boolean {
+		when(id) {
+			0 -> modifier *= 2
+			1 -> modifier /= 2
+			2 -> modifier += 1
+			3 -> modifier -= 1
+			else -> return false
+		}
+		onCraftMatrixChanged(craftMatrix)
+		return true
 	}
 }
--- src/main/kotlin/cpup/mc/workbenchUpgrades/WorkbenchGUI.kt
+++ src/main/kotlin/cpup/mc/workbenchUpgrades/WorkbenchGUI.kt
@@ -1,5 +1,6 @@
 package cpup.mc.workbenchUpgrades
 
+import net.minecraft.client.gui.GuiButton
 import net.minecraft.client.gui.inventory.GuiContainer
 import net.minecraft.client.renderer.GlStateManager
 import net.minecraft.client.resources.I18n
@@ -9,9 +10,21 @@ import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 
 class WorkbenchGUI(inv: InventoryPlayer, world: World, pos: BlockPos): GuiContainer(ContainerWorkbench(inv, world, pos)) {
+	val container = inventorySlots as ContainerWorkbench
+
+	override fun initGui() {
+		super.initGui()
+
+		buttonList.add(GuiButton(0, guiLeft + 133, guiTop + 10, 20, 20, "**"))
+		buttonList.add(GuiButton(1, guiLeft + 133, guiTop + 56, 20, 20, "**"))
+		buttonList.add(GuiButton(2, guiLeft + 113, guiTop + 10, 20, 20, "++"))
+		buttonList.add(GuiButton(3, guiLeft + 113, guiTop + 56, 20, 20, "--"))
+	}
+
 	override fun drawGuiContainerForegroundLayer(p_drawGuiContainerForegroundLayer_1_: Int, p_drawGuiContainerForegroundLayer_2_: Int) {
-		fontRendererObj.drawString(I18n.format("container.crafting", *arrayOfNulls<Any>(0)), 28, 6, 4210752)
-		fontRendererObj.drawString(I18n.format("container.inventory", *arrayOfNulls<Any>(0)), 8, ySize - 96 + 2, 4210752)
+		fontRendererObj.drawString(I18n.format("container.crafting", *arrayOfNulls<Any>(0)), 28, 6, 0x404040)
+		fontRendererObj.drawString(I18n.format("container.inventory", *arrayOfNulls<Any>(0)), 8, ySize - 96 + 2, 0x404040)
+		fontRendererObj.drawStringWithShadow(container.modifier.toString(), 148f, (35 + 18/2 - 10/2).toFloat(), 0xFFFFFF)
 	}
 
 	override fun drawGuiContainerBackgroundLayer(p_drawGuiContainerBackgroundLayer_1_: Float, p_drawGuiContainerBackgroundLayer_2_: Int, p_drawGuiContainerBackgroundLayer_3_: Int) {
@@ -20,6 +33,14 @@ class WorkbenchGUI(inv: InventoryPlayer, world: World, pos: BlockPos): GuiContai
 		val lvt_4_1_ = (width - xSize) / 2
 		val lvt_5_1_ = (height - ySize) / 2
 		drawTexturedModalRect(lvt_4_1_, lvt_5_1_, 0, 0, xSize, ySize)
+	}
+
+	override fun actionPerformed(button: GuiButton?) {
+		super.actionPerformed(button)
+		button!!
+		if(inventorySlots.enchantItem(mc.thePlayer, button.id)) {
+			mc.playerController.sendEnchantPacket(inventorySlots.windowId, button.id)
+		}
 	}
 
 	override fun onGuiClosed() {
